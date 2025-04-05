3c622122859fd6a212b89724380d2ef998ce44d2
CORBIERE Nolan <nocorb.05@gmail.com>
2025-04-05 11:06:40 +0200

    maj site config 4

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,7 @@
 // @ts-check
 import { defineConfig } from 'astro/config'
 
+// https://astro.build/config
 export default defineConfig({
-  site: 'https://LordBabat.github.io',
+  site: 'https://corbierenolan-btssio.netlify.app',
 })
-
